47efe1d9042f39c16155b64667eae50b4efabef6
LFrolov <froleod@gmail.com>
2025-02-18 14:35:41 +0300

    refactor

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,6 @@
 
 const products = [
-    { id: 1, name: 'Товар 1', price: 100, image: '/1.jpg' },
+    { id: 1, name: 'Товар 1', price: 100, image: '/1.jpeg' },
     { id: 2, name: 'Товар 2', price: 200, image: '/2.png' },
     { id: 3, name: 'Товар 3', price: 300, image: '/3.jpeg' },
 ];
